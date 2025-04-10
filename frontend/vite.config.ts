834920f982e473d606305cd7f9eb4d5e99043f5f
rayanjainn <rayansjain@gmail.com>
2025-04-11 00:18:08 +0530

    env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  define: {
+    "process.env.VITE_WS_URL_PROD": JSON.stringify(
+      process.env.VITE_WS_URL_PROD
+    ),
+  },
 });
